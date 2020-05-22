1e6f83080a9918e01a84135b147f37b2cd4078b8
Felipe Osornio <felipe.osrn@gmail.com>
2020-05-22 12:00:27 -0500

    Variables de ambiente agregadas y correción en la petición de crear partida

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,90 +1,84 @@
-import React, {Component} from "react";
-import axios from "axios";
-import M from "materialize-css";
+import React, {Component} from "react"
+import Axios from 'axios'
+import M from "materialize-css"
 import "./Login.css";
 
 class Login extends Component {
-    constructor() {
-        super();
+
+    constructor(props) {
+
+        super(props)
+
         this.state = {
             nombre: "",
-            multijugador: "",
-        };
+            multijugador: false,
+        }
+
     }
 
     componentDidMount() {
-        //Initialize the animations of materialize
-        M.AutoInit();
-    }
 
-    loginUsuario(nombre, multijugador) {
-        // Comunicacion con el servidor
-        fetch("localhost:8080/crear-partida", {
-            method: "POST",
-            body: JSON.stringify({
-                nombre: nombre,
-                multijugador: multijugador
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        /** Initialize the animations of materialize */
+        M.AutoInit()
 
     }
 
-    handleInput = (e) => {
-        const {value, name} = e.target;
-        this.setState({
-            [name]: value
-        });
-        console.log(this.state);
-    };
+    loginUsuario = () => {
+        /** Comunicacion con el servidor */
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        axios({
-            method: 'post',
-            url: 'http://127.0.0.1:8080/crear-partida',
-            data: {
-                nombre: this.state.nombre,
-                multijugador: this.state.multijugador            }
-        });
-        // fetch("http://127.0.0.1:8080/crear-partida", {
-        //     method: "POST",
-        //     body: JSON.stringify({
-        //         nombre: this.state.nombre,
-        //         multijugador: this.state.multijugador
-        //     }),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // });
-    };
+        const {nombre, multijugador} = this.state
+
+        Axios.post(`http://${process.env.REACT_APP_HOST_PORT}/crear-partida`, {
+            nombre: nombre,
+            multijugador: multijugador === 'true'
+        }).then((res) => {
+
+            const {id_partida, multijugador} = res.data
+            console.log(id_partida, multijugador)
+
+        }).catch((err) => {
+
+            console.log(err)
+
+        })
+
+    }
+
+    handleInput = (evt) => {
+
+        evt.preventDefault()
+
+        const {value, name} = evt.target
+
+        this.setState({[name]: value})
+
+    }
+
 
     render() {
         return (
             <>
                 <div className="login-form">
                     <div className="row">
-                        <form onSubmit={this.handleSubmit} className="col s12">
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <textarea name="nombre" onChange={this.handleInput} className="materialize-textarea"/>
-                                    <label htmlFor="textarea1">Nombre</label>
-                                </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <textarea name="nombre" onChange={this.handleInput} className="materialize-textarea"/>
+                                <label htmlFor="textarea1">Nombre</label>
                             </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <select name="multijugador" onChange={this.handleInput}>
-                                        <option disabled selected>Choose your option</option>
-                                        <option value={true} className="white">Si</option>
-                                        <option value={false} className="red-text">No</option>
-                                    </select>
-                                    <label>Multijugador</label>
-                                </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <select name="multijugador" onChange={this.handleInput}>
+                                    <option disabled selected>Choose your option</option>
+                                    <option value={true} className="white">Si</option>
+                                    <option value={false} className="red-text">No</option>
+                                </select>
+                                <label>Multijugador</label>
                             </div>
-                            <button type="submit" className="waves-effect waves-light btn">Entrar</button>
-                        </form>
+                        </div>
+                        <button type="submit" className="waves-effect waves-light btn" onClick={this.loginUsuario}>
+                            Entrar
+                        </button>
                     </div>
                 </div>
             </>
