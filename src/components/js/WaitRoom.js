15b7e08904945867800044461e5dca58159b5070
Ericksad01 <erickburn01@hotmail.com>
2020-06-09 19:57:37 -0500

    "Import of material ui for icons and timer in WaitRoom"

--- src/components/js/WaitRoom.js
+++ src/components/js/WaitRoom.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react'
+import "../css/WaitRoom.css"
 import logo from '../../img/logo-blackjack.png'
 import {Link} from "react-router-dom";
+import ClearIcon from '@material-ui/icons/Clear';
 
 class WaitRoom extends Component {
+    constructor() {
+        super();
+        this.state = {
+            minutes: 1,
+            seconds: 0
+        }
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const {seconds, minutes} = this.state
+            if (seconds > 0) {
+                this.setState(({seconds}) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({minutes}) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            }
+        }, 1000)
+    }
 
     render() {
+        const {minutes, seconds} = this.state
         let jugadores = 0
 
         return (
@@ -37,11 +68,38 @@ class WaitRoom extends Component {
 
                 <div className="container has-margin-top">
                     <ul className="collection with-header">
-                        <li className="collection-header"><h4>Jugadores</h4> </li>
-                        <li className="collection-item">Alvin</li>
-                        <li className="collection-item">Alvin</li>
-                        <li className="collection-item">Alvin</li>
-                        <li className="collection-item">Alvin</li>
+                        <li className="collection-header space-between">
+                            <h4>Jugadores</h4>
+                            {minutes === 0 && seconds === 0
+                                ? <h5>Time out</h5>
+                                : <h5>Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h5>
+                            }
+                        </li>
+                        <li className="collection-item space-between">
+                            <span>Alvin</span>
+                            <a className="btn flex-align-center background-btn">
+                                <ClearIcon style={{color: "black"}}/>
+                            </a>
+
+                        </li>
+                        <li className="collection-item space-between">
+                            <span>Alvin</span>
+                            <a className="btn flex-align-center background-btn">
+                                <ClearIcon style={{color: "black"}}/>
+                            </a>
+                        </li>
+                        <li className="collection-item space-between">
+                            <span>Alvin</span>
+                            <a className="btn flex-align-center background-btn">
+                                <ClearIcon style={{color: "black"}}/>
+                            </a>
+                        </li>
+                        <li className="collection-item space-between">
+                            <span>Alvin</span>
+                            <a className="btn flex-align-center background-btn">
+                                <ClearIcon style={{color: "black"}}/>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </>
